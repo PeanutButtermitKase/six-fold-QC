16a321c0377032eae4e44b9831bfff13d8ca57aa
PeanutButterMitKase <PeanutButterMitKase>
2024-07-29 09:19:49 +0200

    predict cqc probability score

---
+++ predict.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import sys
+
+# Parameters
+inputModel = 'best_chirality_detector_early.keras' # Input name of the model
+sizeImage  = 512         # Image size
+scaleInput = 'grayscale' # Color scale
+
+def predictChirality(img_path):
+    model      = load_model(inputModel)  # load model
+    img        = image.load_img(img_path, target_size=(sizeImage, sizeImage), color_mode=scaleInput) # Load image
+    imgArray   = image.img_to_array(img) # convert to array
+    imgArray   = np.expand_dims(imgArray, axis=0) / 255.0
+    prediction = model.predict(imgArray) # predict
+    return prediction[0][0] # output label
+
+
+img_path            = sys.argv[1]                # Path to image director
+cqcProbabilityScore = predictChirality(img_path) # Predict cqc
+newFile             = open(img_path.replace(".png","_m2.dat"),"w") # set-up output file
+newFile.write(str(cqcProbabilityScore)) # Write prediction to a file FILENAME+"_m2.dat"
